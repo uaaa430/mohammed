43e65c124f3f6c463208616da85704688f26beae
uaaa430 <ferhahmohammedislem@gmail.com>
2025-12-08 18:16:16 +0100

    Update main (3).cpp

--- main (3).cpp
+++ main (3).cpp
@@ -3,92 +3,22 @@
 
 int main()
 {
+ int num_students;
+    cout << "Enter number of students: ";
+    cin >> num_students;
+    vector<Person> students(num_students);
 
-    filegenerator ("students10000.txt",10000);
-    filegenerator ("students100000.txt",100000);
-    filegenerator ("students1000000.txt",1000000);
-    filegenerator ("students10000000.txt",10000000);
+    for (int i = 0; i < num_students; ++i) {
+        cout << "\nEnter data for student #" << i + 1 << ":\n";
+        cin >> students[i];
+    }
 
-    vector <Person> pass;
-    vector <Person> fail;
 
-    Timer timer;
-
-    try {
-        timer.timestart();
-
-        ifstream file("Students.txt");
-cout<<"Time taken to read file : " << endl;
-        timer.timetaken();
-
-        if (!file) {
-            throw runtime_error("Failed to open Students.txt");
-        }
-
-        string header;
-
-       if (!getline(file, header)){ throw runtime_error ( " Failed to read the header row") ; }
-
-        vector<Person> students;
-        Person temp;
-
-        while (file >> temp) {
-            students.push_back(temp);
-        }
-        timer.timestart();
-        // sorting by firstname
-        sort(students.begin(), students.end(), [](const Person& a, const Person& b) {
-            return a.firstname < b.firstname;
-        });
-cout<<"Time taken to sort file : " << endl;
-       timer.timetaken();
-
-        timer.timestart();
-        for (const auto& s : students) {
-            if( (s.FinalgradeAvg >= 5 )|| (s.FinalgradeMed >= 5)  )
-            {
-                pass.push_back(s);
-            }
-            else{ fail.push_back(s);}
-       }
-
-       cout<<"Time taken to split file : " << endl;
- timer.timetaken();
-
-timer.timestart();
-        // Output
-        cout << "PASSED" << endl;
-       outputtemplate();
-
-       ofstream passout("passed.txt"), failedout ( "failed.txt");
-       passout << left << setw(15) << "Name" << setw(15) << "Surname"
-             << setw(20) << "Final (Avg.)" << setw(5) << "|" << "Final (Med.)" << endl;
-
-             failedout<< left << setw(15) << "Name" << setw(15) << "Surname"
-             << setw(20) << "Final (Avg.)" << setw(5) << "|" << "Final (Med.)" << endl;
-
-       for (const auto& s : pass)
-       {
-           passout << s;
-           cout<<s<<endl;
-       }
-       cout << "FAILED" << endl;
-        outputtemplate();
-
-       for (const auto& s : fail)
-       {
-           failedout << s;
-           cout<<s<<endl;
-       }
-
-       cout<<"Time taken to output two new files : " << endl;
- timer.timetaken();
-
-    } catch (const exception& e) {
-        cerr << "Error: " << e.what() << endl;
-        return 1;
+    // Output
+    cout << left << setw(15) << "Name" << setw(15) << "Surname" << setw(20) << "Final (Avg.)" << setw(15) << "Final (Med.)" << endl;
+    cout << "------------------------------------------------------" << endl;
+    for (const auto &s : students) {cout << s << endl;
     }
 
     return 0;
 }
-
