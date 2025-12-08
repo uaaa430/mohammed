7e1e232a1c9a38b5512fe8cd2769194198480313
uaaa430 <ferhahmohammedislem@gmail.com>
2025-12-08 18:15:01 +0100

    Update Person (1) (1).h

--- Person (1) (1).h
+++ Person (1) (1).h
@@ -1,22 +1,29 @@
-#ifndef PERSON_H
-#define PERSON_H
-#include <string>
-#include <vector>
-#include <iostream>
-class Person {
-public:
-    std::string name, surname;
-    std::vector<double> homeworks;
-    double exam, finalGrade;
-    Person();
-    Person(const std::string&, const std::string&);
-    Person(const Person&);
-    Person& operator=(const Person&);
-    ~Person();
-    friend std::istream& operator>>(std::istream&, Person&);
-    friend std::ostream& operator<<(std::ostream&, const Person&);
-    void calculateFinal(bool useMedian) noexcept;
-    double computeAverage() const noexcept;
-    double computeMedian() const noexcept;
+
+
+#include "Lib.h"
+
+class Person
+{
+    public:
+    string firstname, surname;
+    vector <int> Homework;
+    int exam;
+    double FinalgradeAvg;
+    double FinalgradeMed;
+
+
+    void FinalAv();
+     double med();
+    void FinalMed();
+    Person ();
+
+
+    Person (string A, string B, vector<int> C, int D);
+
+    ~Person() ;
+    Person(const Person& other) ;
+    Person& operator=(const Person& other);
+   friend std::ostream& operator<<(std::ostream& os, const Person& p);
+    friend std::istream& operator>>(std::istream& is, Person& p);
+
 };
-#endif
