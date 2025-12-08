9a5ba3b4fad4f254cae1ad3ce1ea533f5482f4cc
uaaa430 <ferhahmohammedislem@gmail.com>
2025-12-08 18:19:56 +0100

    Update Person (1) (1).h

--- include/Person (1) (1).h
+++ include/Person (1) (1).h
@@ -1,22 +1,41 @@
-#ifndef PERSON_H
-#define PERSON_H
-#include <string>
-#include <vector>
-#include <iostream>
-class Person {
+
+
+#include "Lib.h"
+class Timer{
 public:
-    std::string name, surname;
-    std::vector<double> homeworks;
-    double exam, finalGrade;
-    Person();
-    Person(const std::string&, const std::string&);
-    Person(const Person&);
-    Person& operator=(const Person&);
-    ~Person();
-    friend std::istream& operator>>(std::istream&, Person&);
-    friend std::ostream& operator<<(std::ostream&, const Person&);
-    void calculateFinal(bool useMedian) noexcept;
-    double computeAverage() const noexcept;
-    double computeMedian() const noexcept;
+ using clock = std::chrono::high_resolution_clock;
+
+std::chrono::time_point<clock> start, temp;
+   std::chrono::duration <double> executiontime;
+
+void timestart( ) ;
+void timetaken();
 };
-#endif
+void outputtemplate ();
+void filegenerator (const string& filename, size_t n);
+class Person
+{
+    public:
+    string firstname, surname;
+    vector <int> Homework;
+    int exam;
+    double FinalgradeAvg;
+    double FinalgradeMed;
+
+
+    void FinalAv();
+
+     double med();
+    void FinalMed();
+    Person ();
+
+
+    Person (string A, string B, vector<int> C, int D);
+
+    ~Person() ;
+    Person(const Person& other) ;
+    Person& operator=(const Person& other);
+   friend std::ostream& operator<<(std::ostream& os, const Person& p);
+    friend std::istream& operator>>(std::istream& is, Person& p);
+
+};
