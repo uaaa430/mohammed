7db324b8951f96a2db3b26b7c4dee506508dc968
uaaa430 <ferhahmohammedislem@gmail.com>
2025-12-09 17:11:33 +0100

    Refactor Person class and add Timer class

--- Person (1) (1).h
+++ Person (1) (1).h
@@ -1,29 +1,40 @@
-
-
 #include "Lib.h"
 
-class Person
-{
-    public:
-    string firstname, surname;
-    vector <int> Homework;
-    int exam;
-    double FinalgradeAvg;
-    double FinalgradeMed;
+class Timer {
+public:
+    using ClockType = std::chrono::high_resolution_clock;
+    std::chrono::time_point<ClockType> startTime, endTime;
+    std::chrono::duration<double> elapsed;
 
-
-    void FinalAv();
-     double med();
-    void FinalMed();
-    Person ();
-
-
-    Person (string A, string B, vector<int> C, int D);
-
-    ~Person() ;
-    Person(const Person& other) ;
-    Person& operator=(const Person& other);
-   friend std::ostream& operator<<(std::ostream& os, const Person& p);
-    friend std::istream& operator>>(std::istream& is, Person& p);
-
+    void start();
+    void stop();
 };
+
+void printHeader();
+void generateFile(const std::string& filename, size_t n);
+
+class Person {
+public:
+    std::string name;
+    std::string surname;
+    std::vector<int> homework;
+    int examScore;
+    double finalAverage;
+    double finalMedian;
+
+    void calculateAverage();
+    double calculateMedianValue() const;
+    void calculateMedianFinal();
+
+    Person();
+    Person(const std::string& n, const std::string& s,
+           const std::vector<int>& hw, int ex);
+    Person(const Person& other);
+    Person& operator=(const Person& other);
+    ~Person();
+
+    friend std::ostream& operator<<(std::ostream& os, const Person& p);
+    friend std::istream& operator>>(std::istream& is, Person& p);
+};
+
+#endif
