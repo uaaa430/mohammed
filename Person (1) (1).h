b524b73ef39910b7a554b7610b797469fc8f70a3
uaaa430 <ferhahmohammedislem@gmail.com>
2025-12-22 15:26:15 +0100

    Update Person (1) (1).h

--- Person (1) (1).h
+++ Person (1) (1).h
@@ -1,40 +1,32 @@
-#include "Lib.h"
+#ifndef PERSON_H
+#define PERSON_H
 
-class Timer {
-public:
-    using ClockType = std::chrono::high_resolution_clock;
-    std::chrono::time_point<ClockType> startTime, endTime;
-    std::chrono::duration<double> elapsed;
-
-    void start();
-    void stop();
-};
-
-void printHeader();
-void generateFile(const std::string& filename, size_t n);
+#include <string>
+#include <vector>
 
 class Person {
-public:
+private:
     std::string name;
     std::string surname;
     std::vector<int> homework;
-    int examScore;
-    double finalAverage;
-    double finalMedian;
+    int exam;
+    double finalGrade;
 
-    void calculateAverage();
-    double calculateMedianValue() const;
-    void calculateMedianFinal();
-
+public:
     Person();
-    Person(const std::string& n, const std::string& s,
-           const std::vector<int>& hw, int ex);
-    Person(const Person& other);
-    Person& operator=(const Person& other);
+    Person(const Person&);
+    Person& operator=(const Person&);
     ~Person();
 
-    friend std::ostream& operator<<(std::ostream& os, const Person& p);
-    friend std::istream& operator>>(std::istream& is, Person& p);
+    void generateRandom();
+    void calculateFinal();
+
+    double getFinal() const;
+    std::string getName() const;
+    std::string getSurname() const;
+
+    friend std::istream& operator>>(std::istream&, Person&);
+    friend std::ostream& operator<<(std::ostream&, const Person&);
 };
 
 #endif
