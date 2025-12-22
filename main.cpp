ea87d24ee0eb4a0f963ae2afb92b3a63a906a701
uaaa430 <ferhahmohammedislem@gmail.com>
2025-12-22 15:20:32 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include "Person.h"
 #include <vector>
 #include <iostream>
+#include <iomanip>
 
 int main() {
     std::vector<Person> students;
@@ -13,12 +14,22 @@ int main() {
     std::cout << "Number of students: ";
     std::cin >> n;
 
+    std::cout << "Enter data: Name Surname HW1 HW2 ... HWn -1 Exam\n";
+
     for (int i = 0; i < n; i++) {
         Person p;
         std::cin >> p;
         p.calculateFinal(useMedian);
         students.push_back(p);
     }
+
+    std::cout << std::left
+              << std::setw(12) << "Name"
+              << std::setw(15) << "Surname"
+              << "Final_Point"
+              << std::endl;
+
+    std::cout << "----------------------------------------" << std::endl;
 
     for (const auto& s : students) {
         std::cout << s << std::endl;
